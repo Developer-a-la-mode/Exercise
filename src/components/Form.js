60c6ba3c6091b62fa30ef9633d11fc138286a4de
Kevin Tonthat <tonthat.kevin@gmail.com>
2022-06-13 22:04:33 -0700

    username and user age event handlers

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import { React, useState } from 'react';
 import './Form.css';
 
 const Form = () => {
+    const [userName,onSetUserName] = useState(' ')
+    const [userAge,onSetUserAge] = useState(' ')
 
+    const onChangeUserHandler = (event) => {
+        const userName = event.target.value;
+    }
+
+    const onChangeAgeHandler = (event) => {
+        const userAge = event.target.value;
+    }
+
+    const submitHandler = () => {
+        console.log(userAge, userName);
+    }
 
     return (
         <div className="form">
@@ -10,14 +23,14 @@ const Form = () => {
             <label className="form-label">
                 Username
             </label>
-            <input type="text" name="name" className="form-input--text" />
+            <input type="text" name="name" className="form-input--text" onChange={onChangeUserHandler} />
 
             <label className="form-label">
                 Age
             </label>
-            <input type="text" name="name" className="form-input--text" />
+            <input type="text" name="name" className="form-input--text" onChange={onChangeAgeHandler} />
 
-            <input type="submit" className="form-input" value="Add User" />
+            <input type="submit" className="form-input" value="Add User" onSubmit={submitHandler} />
         </form>
         </div>
         
