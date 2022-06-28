2c89edc698b45fa139e9a2d6887983b91c0f897c
Kevin Tonthat <tonthat.kevin@gmail.com>
2022-06-27 23:53:15 -0700

    errormodal backdrop

--- src/components/ErrorModal.js
+++ src/components/ErrorModal.js
@@ -6,21 +6,25 @@ import classes from './ErrorModal.module.css';
 const ErrorModal = props => {
 
     return (
-        <Card className={classes.modal}>
-            <header className={classes.header}>
-                <h2>
-                    {props.title}
-                </h2>
-            </header>
+        <div>
+            <div className={classes.backdrop}>
+                <Card className={classes.modal}>
+                    <header className={classes.header}>
+                        <h2>
+                            {props.title}
+                        </h2>
+                    </header>
 
-            <div className={classes.content}>
-                <p>{props.message}</p>
+                    <div className={classes.content}>
+                        <p>{props.message}</p>
+                    </div>
+
+                    <footer className={classes.actions}>
+                        <Button>Button</Button>
+                    </footer>
+                </Card>
             </div>
-
-            <footer className={classes.actions}>
-                <Button>Button</Button>
-            </footer>
-        </Card>
+        </div>
     )
 }
 
